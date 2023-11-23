702d4a7ae212983c2254333c5c14bc651c83dca8
nasre <nasreensheik365@gmIil.com>
2023-11-23 19:11:30 +0530

    a

--- variables/var.tf
+++ variables/var.tf
@@ -1,8 +1,12 @@
 variable "plain" {
    default= "hello world"
-
 }
-
 output "demo2" {
   value = var.plain
+}
+variable "list" {
+  default = ["AWS","DEVOPS","CLOUD"]
+}
+output "demo3" {
+  value = var.list[0]
 }
