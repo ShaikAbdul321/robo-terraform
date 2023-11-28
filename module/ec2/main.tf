d13251350ce0f7ef5f1e59658b8a5659d846f62a
nasre <nasreensheik365@gmIil.com>
2023-11-28 10:44:35 +0530

    a

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -8,7 +8,6 @@ resource "aws_instance" "web" {
   }
 }
 data "aws_ami" "example" {
-  executable_users = ["self"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
