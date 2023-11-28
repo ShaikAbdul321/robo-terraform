35bfb941dc616be7e396f8acb7ec01c753c406b3
nasre <nasreensheik365@gmIil.com>
2023-11-28 13:14:04 +0530

    a

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,17 +1,42 @@
+##############create ec2 instance terraform################
 resource "aws_instance" "web" {
   ami  = data.aws_ami.example.id
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.sg.name]
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = var.name
   }
 }
+
+resource "null_resource" "null" {
+  depends_on = [aws_instance.web,aws_route53_record.www]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.web.public_ip
+    }
+    inline = [
+      "sudo set-hostname ${var.name}"
+    ]
+  }
+}
+resource "aws_route53_record" "www" {
+  zone_id = "Z08631072FSVS0GDIGH1F"
+  name    = "${var.name}-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.web.private_ip]
+}
+###########ami id datasource terraform##############
 data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
+#################security group terraform####################
 resource "aws_security_group" "sg" {
   name        = var.name
   description = "Allow TLS inbound traffic"
--- resource/main.tf
+++ resource/main.tf
@@ -1,3 +1,4 @@
+##############create ec2 instance terraform################
 resource "aws_instance" "web" {
   ami  = data.aws_ami.example.id
   instance_type = "t2.micro"
@@ -7,11 +8,13 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+###########ami id datasource terraform##############
 data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
  }
+#################security group terraform####################
 resource "aws_security_group" "sg" {
   name        = "sallow-all"
   description = "Allow TLS inbound traffic"
